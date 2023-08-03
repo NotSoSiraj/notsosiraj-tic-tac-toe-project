913a3e86469250a92ae85cbf471000154a299a31
SRJ <iamsirajsiyal@gmail.com>
2023-08-03 10:13:10 +0500

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import './styles.scss'
+import './styles.scss';
 import Board from './components/Board';
+import { useState } from 'react';
+
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  console.log('hello');
+  const onBtnClick = () => {
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
+
   return (
-    <div className='app'>
-      <Board />
+    <div className="app">
+      <button onClick={onBtnClick}>Click me plz</button>
+      <div>{counter}</div>
     </div>
-  )
+  );
 }
 
 export default App;
